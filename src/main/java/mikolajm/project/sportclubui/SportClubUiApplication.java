cd92ce15948a00af027233d185be6426eb109a4d
Promess02 <miko20020921@gmail.com>
2023-11-22 11:26:36 +0100

    divided into two modules

--- src/main/java/mikolajm/project/sportclubui/SportClubUiApplication.java
+++ src/main/java/mikolajm/project/sportclubui/SportClubUiApplication.java
@@ -1,6 +1,5 @@
 package mikolajm.project.sportclubui;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
